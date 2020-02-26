5d96a768af0ed5ca32ef69dd43e8881c79756338
Deijah Price <deijahwilson1@gmail.com>
2020-02-26 22:15:08 +0000

    Automatically backed up by Learn

--- lib/nested_array.rb
+++ lib/nested_array.rb
@@ -35,11 +35,15 @@ end
 //
 
 def matrix_lookup(matrix, row, column)
+  
   matrix[row][column]
+  
 end
 
 //
 
 def matrix_update(matrix, row, column, new_value)
+  
   matrix[row][column]=new_value
+  
 end
